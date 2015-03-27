d304e840c77a5296d13e129f3630530e1a8d9bdc
David Dérus <derus.david@gmx.fr>
2015-03-28 00:48:24 +0100

    [*] Better requires and showing the right version

--- lib/asana_change_logger/cli.rb
+++ lib/asana_change_logger/cli.rb
@@ -1,13 +1,13 @@
 require 'Slop'
-require 'asana'
-require 'export'
+require_relative 'asana'
+require_relative 'export'
 
 module AsanaChangeLogger
   opts = Slop.parse do |o|
     o.string '-h', '--host', 'a hostname'
     o.integer '--port', 'custom port', default: 80
     o.on '--version', 'print the version' do
-      puts Slop::VERSION
+      puts AsanaChangeLogger::VERSION
       exit
     end
   end
