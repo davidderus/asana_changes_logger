a9a086fb3f5258d261b71619f72244841abf56d2
David Dérus <derus.david@gmx.fr>
2015-03-28 16:48:27 +0100

    [+] Adding template handler to render plaintext output

--- lib/asana_change_logger/export.rb
+++ lib/asana_change_logger/export.rb
@@ -1,5 +1,8 @@
+require 'erb'
+
 module AsanaChangeLogger
   class Exporter
+
     def initialize(tasks)
       @list_tasks = tasks
       @as_tasks = []
@@ -12,6 +15,11 @@ module AsanaChangeLogger
     end
 
 
+    def group_by(key)
+
+    end
+
+
     def ready?
       @as_tasks && @as_tasks.length > 0
     end
@@ -19,39 +27,53 @@ module AsanaChangeLogger
 
     def to_markdown
       raise 'Not ready' unless ready?
-      @as_tasks.join('\r\n')
+      @as_tasks.join("\r\n")
     end
 
 
     def to_html
       raise 'Not ready' unless ready?
-      @as_tasks.join('\r\n')
+      @as_tasks.join("\r\n")
     end
 
 
-    def to_pdf
+    def to_plaintext
       raise 'Not ready' unless ready?
-      @as_tasks.join('\r\n')
+      
+      render('plaintext.txt')
     end
 
 
     def to_term
-      raise 'Not ready' unless ready?
-      puts @as_tasks
+      puts to_plaintext
     end
 
 
     def save(dest, format)
       case format
-      when 'pdf'
-        content = to_pdf
+      when 'md'
+        content = to_markdown
       when 'html'
         content = to_html
       else
-        content = to_markdown
+        content = to_plaintext
       end
 
       File.open(dest, 'w') { |file| file.write(content) }
+    end
+
+
+    private
+
+
+    def read_template(tpl_name)
+      File.read(File.expand_path(File.dirname(__FILE__)) + "/tpl/#{tpl_name}.erb")
+    end
+
+
+    def render(template)
+      renderer = ERB.new(read_template(template), nil, '>')
+      renderer.result(binding)
     end
   end
 end
