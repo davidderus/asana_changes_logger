20b2b69ccbe9a92d84b7de9fc345f8094ae7c04e
David Dérus <derus.david@gmx.fr>
2015-03-28 01:46:03 +0100

    [+] Checking for auth but not versionning config file

--- lib/asana_change_logger.rb
+++ lib/asana_change_logger.rb
@@ -5,13 +5,15 @@ require 'asana_change_logger/export'
 require 'asana_change_logger/config'
 
 module AsanaChangeLogger
-  def auth?
 
+  def self.auth?
+    defined?(APP_CONFIG) && APP_CONFIG[:api_key]
   end
 
   if AsanaChangeLogger::OPTS[:days]
+    # Checking for auth
+    raise 'Auth error' unless auth?
     # Should get tasks for said days
-
     if AsanaChangeLogger::OPTS[:output]
       # Should store output in file
     else
