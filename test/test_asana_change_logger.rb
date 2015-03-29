df7985241d78cdecdfec1fe4a36aaeb9a2d996ba
David Dérus <derus.david@gmx.fr>
2015-03-29 18:12:06 +0200

    [*] Trying to write tests

--- test/test_asana_change_logger.rb
+++ test/test_asana_change_logger.rb
@@ -2,18 +2,11 @@ require 'minitest_helper'
 require 'minitest/spec'
 require 'minitest/autorun'
 
-class TestAsanaChangeLogger < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::AsanaChangeLogger::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
-end
-
 describe AsanaChangeLogger do
- it "can be fun" do
-  true.must_be_same_as false
- end
+  before do
+  end
+
+  it "show help" do
+
+  end
 end
