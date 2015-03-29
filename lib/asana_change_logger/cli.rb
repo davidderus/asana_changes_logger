b98f4654fec137d795744c333563448b523b2be4
David Dérus <derus.david@gmx.fr>
2015-03-29 01:56:24 +0100

    [+] Adding api key argument to cli

--- lib/asana_change_logger/cli.rb
+++ lib/asana_change_logger/cli.rb
@@ -5,6 +5,7 @@ module AsanaChangeLogger
     o.integer '-d', '--days', 'Number of days to get', default: 5
     o.integer '-p', '--project', 'Project ID'
     o.string '-o', '--output', 'The output file. Allowed formats: html, md, txt'
+    o.string '-a', '--api', 'Store the given API key and start using it from now'
     o.bool '-lr', '--log-remaining', 'Log remainins tasks too', default: false
     o.on '-v', '--version', 'Print the current version' do
       puts AsanaChangeLogger::VERSION
